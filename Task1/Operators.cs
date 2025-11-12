0240ea4efc477c7c3f3bc02722b020a0247222b8
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-11-12 12:16:47 +0545

    adding task 1 code

---
+++ Task1/Operators.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Operators
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Sum: " + (a + b));
+        }
+
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine("Difference: " + (a - b)); 
+        }
+
+        public void Multiply (int a, int b)
+        {
+            Console.WriteLine("Multiplication: " + (a * b));
+        }
+
+        public void Divide (int a, int b)
+        {
+            Console.WriteLine("Dividend: " + (a / b));
+        }
+
+        public void OddEvenFinder (int number)
+        {
+            string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operators op = new Operators();
+
+            op.Add(7, 8);
+            op.Subtract(19, 10);
+            op.Multiply(5, 6);
+            op.Divide(12, 3);
+            op.OddEvenFinder(7);
+            op.OddEvenFinder(52);
+        }
+    }
+}
