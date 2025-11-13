03b1b898d5f982cdab759c4906afe02ce6c78341
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-11-13 12:03:09 +0545

    Added Task 3 Code

---
+++ Task3/AgeCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class AgeCheck
+    {
+        public void CheckAge()
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 13)
+                Console.WriteLine("You are a child.");
+            else if (age < 20)
+                Console.WriteLine("You are a teenager.");
+            else if (age < 60)
+                Console.WriteLine("You are an adult.");
+            else
+                Console.WriteLine("You are a senior citizen.");
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AgeCheck ageCheck = new AgeCheck();
+
+            Console.WriteLine("If-Else-If Demo");
+            ageCheck.CheckAge();
+
+            Console.WriteLine("Age check completed successfully!");
+        }
+    }
+}
