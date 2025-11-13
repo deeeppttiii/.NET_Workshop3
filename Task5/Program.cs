61504447cefc00f889fa89217d5f9475b8737c55
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-11-13 12:17:57 +0545

    Added Task 5 Code

---
+++ Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loop loops = new Loop();
+
+            Console.WriteLine("Loop");
+            loops.SumFrom1ToN();
+            loops.WhileLoop();
+            loops.ArraySum();
+
+            Console.WriteLine("loop examples executed successfully!");
+        }
+    }
+}
